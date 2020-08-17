062fa3eb9507679759cdb4127f90ffff46dad9f8
DouglasPatton <douglaspatton@gmail.com>
2020-08-17 14:23:54 -0400

    fixes to interact-x and adding two new noteboooks

--- vb_helper.py
+++ vb_helper.py
@@ -41,7 +41,7 @@ class VBHelper:
             ax.xaxis.set_ticks([])
             ax.xaxis.set_visible(False)
             ax.legend(loc=4)
-            fig.show()
+            #fig.show()
             
             
 
@@ -59,9 +59,11 @@ class shrinkBigKTransformer(BaseEstimator,TransformerMixin):
             selector=Lars(fit_intercept=1,normalize=1,n_nonzero_coefs=self.max_k)
         elif self.selector=='elastic-net':
             selector=ElasticNetCV()
+        else:
+            selector=self.selector
         k=X.shape[1]
         selector.fit(X,y)
-        self.col_select=np.arange(k)[selector.coef_>0]
+        self.col_select=np.arange(k)[np.abs(selector.coef_)>0]
         return self
     
     def transform(self,X):
@@ -93,7 +95,7 @@ class logp1_T(BaseEstimator,TransformerMixin):
         return self
     
     def transform(self,X,y=None):
-        
+        X[X<-self.min_shift_]=-self.min_shift_ # added to avoid np.log(neg)
         return np.log1p(X+self.min_shift_)
         
     def inverse_transform(self,X,y=None):
@@ -105,7 +107,7 @@ class logminus_T(BaseEstimator,TransformerMixin):
     def fit(self,X,y=None):
         return self
     def transform(self,X,y=None):
-        return np.sign(y)*np.log(np.abs(y))
+        return np.sign(X)*np.log(np.abs(X))
     def inverse_transform(self,X,y=None):
         return np.exp(X)
            
