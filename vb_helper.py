ce4e6734aafb6f91334544bf0e4621d82b779310
Douglas Patton <douglaspatton@gmail.com>
2020-08-26 16:07:29 -0400

    debugging missing valhandler

--- vb_helper.py
+++ vb_helper.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 from sklearn.linear_model import ElasticNetCV, LinearRegression, Lars
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -6,6 +7,8 @@ from sklearn.metrics import mean_squared_error, make_scorer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import make_pipeline
+from sklearn.impute import SimpleImputer,KNNImputer
+
 
 class VBHelper:
     def __init__(self,test_share,cv_folds,cv_reps,cv_count,rs):
@@ -51,25 +54,33 @@ class missingValHandler(BaseEstimator,TransformerMixin):
         self.strategy=strategy
         
     def fit(self,X,y):
-        return self
-        
-    def transform(self,X):
         if type(X)!=pd.DataFrame:
             X=pd.DataFrame(X)
-        self.X_dtypes_=dict(X_df.dtypes)
+        self.X_dtypes_=dict(X.dtypes)
+        
+        #self.obj_idx=[i for i,(var,dtype) in enumerate(self.X_dtypes_.items()) if dtype=='object']
+        
+        
         self.obj_idx_=[i for i,(var,dtype) in enumerate(self.X_dtypes_.items()) if dtype=='object']
         self.float_idx_=[i for i in range(X.shape[1]) if i not in self.obj_idx_]
-            
+        self.cat_list=[X.iloc[:,idx].unique() for idx in self.obj_idx_]
+        return self
+        
+    def transform(self,X,y=None):
+        if type(X)!=pd.DataFrame:
+            X=pd.DataFrame(X)
+        
+        cat_encoder=OneHotEncoder(categories=self.cat_list,sparse=False,) # drop='first'
         xvars=list(X.columns)
         if type(self.strategy) is str:
             if self.strategy=='drop_row':
                 X=X.dropna(axis=0) # overwrite it
                 numeric_T=('no_transform',self.none_T,self.float_idx_)
-                categorical_T=('cat_drop_hot',OneHotEncoder(sparse=False,drop=None),self.obj_idx)
+                categorical_T=('cat_drop_hot',cat_encoder,self.obj_idx)
                 
             if self.strategy=='impute_middle':
                 numeric_T=('num_imputer', SimpleImputer(strategy='mean'),self.float_idx_)
-                cat_imputer=make_pipeline(SimpleImputer(strategy='median'),OneHotEncoder(sparse=False,drop=None))
+                cat_imputer=make_pipeline(SimpleImputer(strategy='most_frequent'),cat_encoder)
                 categorical_T=('cat_imputer',cat_imputer,self.obj_idx_)
             if self.strategy[:10]=='impute_knn':
                 if len(self.strategy)==10:
@@ -77,7 +88,7 @@ class missingValHandler(BaseEstimator,TransformerMixin):
                 else:
                     k=int(''.join([char for char in self.strategy[10:] if char.isdigit()]))
                 numeric_T=('num_imputer', KNNImputer(strategy='mean'),self.float_idx_)
-                cat_imputer=make_pipeline(SimpleImputer(strategy='median'),OneHotEncoder(sparse=False,drop=None))
+                cat_imputer=make_pipeline(SimpleImputer(strategy='most_frequent'),cat_encoder)
                 categorical_T=('cat_imputer',cat_imputer,self.obj_idx_)
         T=ColumnTransformer(transformers=[numeric_T,categorical_T])
         T.fit(X,y)
