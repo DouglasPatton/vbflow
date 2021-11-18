ec7a45b62cdd883cdc0dff2c6218a5bcbabf6f93
douglaspatton@gmail.com <douglaspatton@gmail.com>
2021-11-18 13:14:09 -0500

    starting prediction intervals

---
+++ vb_pi.py
@@ -0,0 +1,17 @@
+# from http://www.stat.uchicago.edu/~rina/jackknife/jackknife_simulation.html
+
+class CVPlus:
+    def __init__(self,):
+        pass
+    def run(
+        
+        
+        
+if __name__=="__main__":
+    beta=np.randon.normal(size=3)*3
+    X=np.random.normal(size=(100,3)
+    y=X@beta+np.random.normal(size=100)
+    Xpredict=np.randon.normal(size=(4,3))
+    
+    CVPlus().run(
+    
